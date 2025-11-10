4cf4a0211fa74ed436139f9e1a0de526696719c1
hackaholic <juniourkawasakicool@gmail.com>
2025-11-10 13:51:22 +0000

    tavily search

--- react_search_agent.py
+++ react_search_agent.py
@@ -4,21 +4,27 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
+#from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+
 
 load_dotenv()
 
-@tool
-def search(query: str) -> str:
-    """
-    Tools that searches over internet
+# tavily = TavilyClient()
+
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tools that searches over internet
     
-    Args:
-        query: The query to search
-    return:
-        The search result
-    """
-    print(f"searching for {query}")
-    return "Tokyo weather is sunny"
+#     Args:
+#         query: The query to search
+#     return:
+#         The search result
+#     """
+#     print(f"searching for {query}")
+#     #return "Tokyo weather is sunny"
+#     return tavily.search(query=query)
 
 
 llm = ChatGoogleGenerativeAI(
@@ -29,7 +35,7 @@ llm = ChatGoogleGenerativeAI(
         stop=None,
     )
 
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
